81f7ca0c8de5b29e24fc6cacb3a9f6c43b663609
Mmaaay <mazenyousef.my@gmail.com>
2024-02-06 11:00:26 +0200

    fixed the conditional logic in App.tsx for add hotel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider , Navigate } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
@@ -8,7 +8,6 @@ import React from "react";
 
 function App() {
   const { isLoggenIn } = useAppContext();
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -18,15 +17,16 @@ function App() {
           path: "/register",
           element: <Register />,
         },
-        { 
-          path: "/sign-in", 
-          element: <SignIn /> },
-
-          isLoggenIn && {
-          path : `/add-hotel`,
-          element : <AddHotel />
+        {
+          path: "/sign-in",
+          element: <SignIn />,
         },
 
+        {
+          path: "/add-hotel",
+
+          element: isLoggenIn ? <AddHotel /> : <Navigate to
+        },
       ],
     },
 
