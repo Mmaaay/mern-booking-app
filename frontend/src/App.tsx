df0e00e3c13557afd36dcbb0ed6b4ac0dbfa4126
Mmaaay <mazenyousef.my@gmail.com>
2024-02-06 10:47:39 +0200

    fixed AddHotel import name in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
-<<<<<<< HEAD
 import AddHotel from "./pages/AddHotel";
-=======
-import AddHotel from "./pages/addHotel";
->>>>>>> 5b0c85a653bff5f6fd0a7ba4a000634f67edf4d2
 import { useAppContext } from "./context/AppContext";
 import React from "react";
 
